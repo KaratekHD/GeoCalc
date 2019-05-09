b08332010643efb402c5661f7b0e54e79a802873
Karatek_HD <jens@krieger-juhnke.de>
2019-05-09 19:00:07 +0200

    Added support for english language

--- src/com/karatek/geocalc/main/Main.java
+++ src/com/karatek/geocalc/main/Main.java
@@ -20,7 +20,7 @@ public class Main {
             }
         }
         System.out.println("Running GeoCalc " + version + " on " + System.getProperty("os.name") + ".");
-        System.out.println("Written by Karatek_HD. All rights reserved.");
+        System.out.println("Written by Karatek_HD. Licensed under Gnu General Public Licence 3 (GPLv3)");
         con();
     }
 
@@ -42,6 +42,7 @@ public class Main {
                 System.out.println("tri - Flächeninhalt eines Dreiecks berechnen.");
                 System.out.println("rect - Flächeninhalt eines Rechtecks berechnen.");
                 System.out.println("tra - Flächeninhalt eines Trapezes berechnen.");
+                System.out.println("pri - Volumen eines Prismas berechnen.");
                 System.out.println("echo - Gibt einen Text aus -> echo <Text>");
                 System.out.println("clear - Leert die Konsole.");
                 System.out.println("exit - Diese Sitzung beenden.");
@@ -134,12 +135,17 @@ public class Main {
         } else {
             if(s.equalsIgnoreCase("help")) {
                 System.out.println("List of available commands: ");
+                System.out.println("tri - calculate the area size of an triangle.");
+                System.out.println("tra - calculate the area size of an triangle.");
+                System.out.println("rect - calculate the area size of an triangle.");
+                System.out.println("pri - calculate the area size of an triangle.");
                 System.out.println("help - show this list.");
                 System.out.println("echo - Prints a line -> echo <text>.");
                 System.out.println("clear - Clears the console.");
                 System.out.println("exit - Exit this session. ");
                 con();
             }
+
             if(s.equalsIgnoreCase("exit")) {
                 System.out.println("Have a nice day!");
                 System.exit(0);
@@ -147,6 +153,76 @@ public class Main {
             if(s.equalsIgnoreCase("clear")) {
                 System.out.print("\033[H\033[2J");
                 System.out.flush();
+                con();
+            } if(s.equalsIgnoreCase("tri")) {
+                System.out.println("For calculating the Area Size of an triangle, please answer the following questions: (Numbers without Units!)");
+                String ground = console.readLine("Groundside (g) : ");
+                String high = console.readLine("High (h) : ");
+                if(ground.contains(",") || high.contains(",")) {
+                    System.out.println("Please use dots (.) instead of commas (,)");
+                    con();
+                }
+                double g = Double.parseDouble(ground);
+                double h = Double.parseDouble(high);
+                double result = g * h / 2;
+                System.out.println("Result: " + result);
+                con();
+            }
+            if(s.equalsIgnoreCase("rect")) {
+                System.out.println("For calculating the Area Size of an rectangle, please answer the following questions: (Numbers without Units!)");
+                String ground = console.readLine("side a (a) : ");
+                String high = console.readLine("side b (b) : ");
+                if(ground.contains(",") || high.contains(",")) {
+                    System.out.println("Please use dots (.) instead of commas (,)");
+                    con();
+                }
+                double a = Double.parseDouble(ground);
+                double b = Double.parseDouble(high);
+                double result = a * b;
+                System.out.println("Result: " + result);
+                con();
+            }
+            if(s.equalsIgnoreCase("tra")) {
+                System.out.println("For calculating the Area Size of an trapeze, please answer the following questions: (Numbers without Units!)");
+                String sidea = console.readLine("side a (a) : ");
+                String sidec = console.readLine("side c (c) : ");
+                String high = console.readLine("high (h): ");
+                if(sidea.contains(",") || sidec.contains(",") || high.contains(",")) {
+                    System.out.println("Please use dots (.) instead of commas (,)");
+                    con();
+                }
+                double a = Double.parseDouble(sidea);
+                double c = Double.parseDouble(sidec);
+                double h = Double.parseDouble(high);
+                double result = ((a + c) / 2 * h);
+                System.out.println("Result: " + result);
+                con();
+            }
+            if(s.equalsIgnoreCase("pri")) {
+
+                System.out.println("Is the area size already known? (y/n)");
+                String aiskn = console.readLine("prism > options > ");
+                if(aiskn.equalsIgnoreCase("y")) {
+                    System.out.println("Please insert the need values:");
+                    String fläche = console.readLine("prism > groundflat > ");
+                    String high = console.readLine("prism > high > ");
+                    if(high.contains(",") || fläche.contains(",")) {
+                        System.out.println("Please use dots (.) instead of commas (,)");
+                        con();
+                    }
+                    double g = Double.parseDouble(fläche);
+                    double h = Double.parseDouble(high);
+                    double result = g * h;
+                    System.out.println("Result : " + result);
+                    con();
+                }
+                if(aiskn.equalsIgnoreCase("n")) {
+                    System.out.println("Please use the commands 'tri', 'rect' und 'tra'.");
+                    con();
+                } else {
+                    System.out.println("That was an invalid input.");
+                    con();
+                }
                 con();
             }
             if(s.startsWith("echo")) {
